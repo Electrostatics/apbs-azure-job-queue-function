436fcd683690f699f4cfb3b70fd83227cbcb7c2e
Ethan Holz <ethan.holz@omsf.io>
2025-02-12 11:36:29 -0700

    feat: test using dynamic job type

--- function_app.py
+++ function_app.py
@@ -6,14 +6,16 @@ app = func.FunctionApp(http_auth_level=func.AuthLevel.ANONYMOUS)
 
 @app.blob_trigger(
     arg_name="blob",
-    path="inputs/{date}/{job}/{name}",
+    path="inputs/{date}/{job}/{jobtype}-job.json",
     connection="BlobStorageConnectionString",
     Source="EventGrid",
 )
 @app.blob_output(arg_name="outputBlob", path="outputs/{date}/{job}/{name}", connection="BlobStorageConnectionString",)
 @app.blob_output(arg_name="inFile", path="outputs/{date}/{job}/{job}.in", connection="BlobStorageConnectionString")
-def BlobTrigger(blob: func.InputStream, outputBlob: func.Out[str], inFile: func.Out[str]):
+@app.blob_output(arg_name="metrics", path="outputs/{date}/{job}/{jobtype}-metrics.json", connection="BlobStorageConnectionString")
+def BlobTrigger(blob: func.InputStream, outputBlob: func.Out[str], inFile: func.Out[str], metrics: func.Out[str]):
     logging.info("BlobTrigger function processed blob\n")
     logging.info(f"{blob.name}\n")
     outputBlob.set(blob.read().decode("utf-8"))
     inFile.set("TESTING this")
+    metrics.set("Metrics")
