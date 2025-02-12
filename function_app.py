50003696eceab6a8174de910865a49e97a0606e5
Ethan Holz <ethan.holz@omsf.io>
2025-02-11 17:26:14 -0700

    add EventGrid

--- function_app.py
+++ function_app.py
@@ -5,7 +5,10 @@ app = func.FunctionApp(http_auth_level=func.AuthLevel.ANONYMOUS)
 
 
 @app.blob_trigger(
-    arg_name="blob", path="outputs/{name}", connection="BlobStorageConnectionString"
+    arg_name="blob",
+    path="outputs/{name}",
+    connection="BlobStorageConnectionString",
+    Source="EventGrid",
 )
 def BlobTrigger(blob: func.InputStream):
     logging.info("BlobTrigger function processed blob\n")
