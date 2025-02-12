e2573d5660f88eb1f6446c2291c8b51044de3593
Ethan Holz <ethan.holz@omsf.io>
2025-02-12 12:21:50 -0700

    feat: try a different way of managing dependencies

--- function_app.py
+++ function_app.py
@@ -1,18 +1,23 @@
 import azure.functions as func
+import azurefunctions.extensions.bindings.blob as blob
 import logging
 
 app = func.FunctionApp(http_auth_level=func.AuthLevel.ANONYMOUS)
 
 
 @app.blob_trigger(
-    arg_name="blob",
+    arg_name="client",
     path="inputs/{date}/{job}/{jobtype}-job.json",
     connection="BlobStorageConnectionString",
     Source="EventGrid",
 )
-@app.blob_output(arg_name="inFile", path="outputs/{date}/{job}/{job}.in", connection="BlobStorageConnectionString")
-@app.blob_output(arg_name="metrics", path="outputs/{date}/{job}/{jobtype}-metrics.json", connection="BlobStorageConnectionString")
-def BlobTrigger(blob: func.InputStream, outputBlob: func.Out[str], inFile: func.Out[str], metrics: func.Out[str]):
-    logging.info(f"{blob.name}\n")
-    inFile.set("TESTING this")
-    metrics.set("Metrics")
+def BlobTrigger(client: blob.BlobClient):
+    sdk = client.get_sdk_type()
+    if sdk:
+        logging.info(f"{sdk.get_blob_properties()}")
+# @app.blob_output(arg_name="inFile", path="outputs/{date}/{job}/{job}.in", connection="BlobStorageConnectionString")
+# @app.blob_output(arg_name="metrics", path="outputs/{date}/{job}/{jobtype}-metrics.json", connection="BlobStorageConnectionString")
+# def BlobTrigger(blob: func.InputStream, outputBlob: func.Out[str], inFile: func.Out[str], metrics: func.Out[str]):
+#     logging.info(f"{blob.name}\n")
+#     inFile.set("TESTING this")
+#     metrics.set("Metrics")
