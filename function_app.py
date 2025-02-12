2f19fe4b9aae728492c10b4a0f2bc016fd31ae3d
Ethan Holz <ethan.holz@omsf.io>
2025-02-11 17:47:00 -0700

    test an output

--- function_app.py
+++ function_app.py
@@ -6,10 +6,12 @@ app = func.FunctionApp(http_auth_level=func.AuthLevel.ANONYMOUS)
 
 @app.blob_trigger(
     arg_name="blob",
-    path="outputs/{name}",
+    path="inputs/{date}/{name}",
     connection="BlobStorageConnectionString",
     Source="EventGrid",
 )
-def BlobTrigger(blob: func.InputStream):
+@app.blob_output(arg_name="outputBlob", path="outputs/{date}/{name}", connection="BlobStorageConnectionString",)
+def BlobTrigger(blob: func.InputStream, outputBlob: func.Out[str]):
     logging.info("BlobTrigger function processed blob\n")
     logging.info(f"{blob.name}\n")
+    outputBlob.set(blob.read().decode("utf-8"))
