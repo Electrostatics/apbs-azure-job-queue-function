87c2bd7c1dd3afd59fac0bb16b7546578fd83923
Ethan Holz <ethan.holz@omsf.io>
2025-02-12 11:57:59 -0700

    feat: test adding another function

--- function_app.py
+++ function_app.py
@@ -10,12 +10,20 @@ app = func.FunctionApp(http_auth_level=func.AuthLevel.ANONYMOUS)
     connection="BlobStorageConnectionString",
     Source="EventGrid",
 )
-@app.blob_output(arg_name="outputBlob", path="outputs/{date}/{job}/{name}", connection="BlobStorageConnectionString",)
 @app.blob_output(arg_name="inFile", path="outputs/{date}/{job}/{job}.in", connection="BlobStorageConnectionString")
 @app.blob_output(arg_name="metrics", path="outputs/{date}/{job}/{jobtype}-metrics.json", connection="BlobStorageConnectionString")
 def BlobTrigger(blob: func.InputStream, outputBlob: func.Out[str], inFile: func.Out[str], metrics: func.Out[str]):
-    logging.info("BlobTrigger function processed blob\n")
     logging.info(f"{blob.name}\n")
-    outputBlob.set(blob.read().decode("utf-8"))
     inFile.set("TESTING this")
     metrics.set("Metrics")
+
+
+@app.blob_trigger(
+    arg_name="pdb",
+    path="inputs/{date}/{job}/{pdb}.pdb",
+    connection="BlobStorageConnectionString",
+)
+@app.blob_output(arg_name="outputBlob", path="outputs/{date}/{job}/{pdb}.pdb", connection="BlobStorageConnectionString")
+def PDBTrigger(pdb: func.InputStream, outputBlob: func.Out[str]):
+    logging.info(f"{pdb.name}\n")
+    outputBlob.set(pdb.read().decode("utf-8"))
