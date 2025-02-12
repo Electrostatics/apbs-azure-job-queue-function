a03da2ddb779059db818b3826d7a8e92717a435d
Ethan Holz <ethan.holz@omsf.io>
2025-02-11 17:06:34 -0700

    feat: remove extensions

--- function_app.py
+++ function_app.py
@@ -1,17 +1,12 @@
 import azure.functions as func
-import azurefunctions.extensions.bindings.blob as blob
 import logging
 
 app = func.FunctionApp(http_auth_level=func.AuthLevel.ANONYMOUS)
 
 
 @app.blob_trigger(
-    arg_name="client", path="outputs/{name}", connection="BlobStorageConnectionString"
+    arg_name="blob", path="outputs/{name}", connection="BlobStorageConnectionString"
 )
-def BlobTrigger(client: blob.BlobClient):
-    sdk = client.get_sdk_type()
-    if sdk is None:
-        logging.error("Blob SDK is None")
-    else:
-        logging.info("Processing\n")
-        logging.info(f"Propertes: {sdk.get_blob_properties()}\n")
+def BlobTrigger(blob: func.InputStream):
+    logging.info("BlobTrigger function processed blob\n")
+    logging.info(f"{blob.name}\n")
